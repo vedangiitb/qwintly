b249a0c29a62f53d5f21010062424d98f99cdf70
vedangiitb <vedangbaleiitb@gmail.com>
2026-02-07 17:47:46 +0530

    added eslint ignore

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // eslint: {
-  //   ignoreDuringBuilds: true, // <— THIS disables ESLint from blocking builds
-  // },
+  eslint: {
+    ignoreDuringBuilds: true, // <— THIS disables ESLint from blocking builds
+  },
 };
 
 export default nextConfig;
