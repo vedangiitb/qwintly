341f1a7fd6402d8aa3cd20adc6a0cb4fe9456cce
vedangiitb <vedangbaleiitb@gmail.com>
2026-03-06 11:40:00 +0530

    auth ui hooks refractor

---
+++ src/features/auth/ui/hooks/useGoogleLogin.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { useCallback } from "react";
+import { googleLogin } from "../../server/services/auth.service";
+import { useAuth } from "./useAuth";
+
+export const useGoogleLogin = () => {
+  const { setError } = useAuth();
+
+  const login = useCallback(async () => {
+    try {
+      setError("");
+      await googleLogin();
+    } catch (err: any) {
+      setError(err.message);
+    }
+  }, []);
+
+  return { login };
+};
