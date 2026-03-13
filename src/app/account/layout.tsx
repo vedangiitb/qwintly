e0fd0422ac006bedd3060d7607ba549db87381ec
vedangiitb <vedangbaleiitb@gmail.com>
2026-03-13 11:40:00 +0530

    account page refractor

---
+++ src/app/account/layout.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+type AccountLayoutProps = {
+  children: ReactNode;
+};
+
+export default function AccountLayout({ children }: AccountLayoutProps) {
+  return children;
+}
